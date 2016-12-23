bfa5fa5051edb0c296f268290509008c7a50565d
Zack <zackakil94@gmail.com>
2016-12-23 18:30:31 +0000

    Update gauge.py

--- gauge.py
+++ gauge.py
@@ -2,13 +2,16 @@ from SimpleCV import *
 import time
 import numpy as np
 
+frame_spec = [160, 120]
+frame_bar = np.array(range(frame_spec[0]))
+bar_height = 10
+frame_roof = (frame_spec[1]/2) - bar_height
+frame_floor = (frame_spec[1]/2) + bar_height
 
 def gauge(frame):
 	nums = frame.getNumpy()
-	slice = (nums[:,230:250,0] > 1)
+	slice = (nums[:,frame_roof:frame_floor,0] > 1)
 	return slice
-
-frame_bar = np.array(range(640))
 
 def get_avg_x(view_slice):
 	count = view_slice.sum()
@@ -17,22 +20,20 @@ def get_avg_x(view_slice):
 
 def draw_box(img, pos):
     overlay = DrawingLayer((img.width, img.height))
-    overlay.centeredRectangle((pos,240),(50,50), color=Color.GREEN, filled=True)
+    overlay.centeredRectangle((pos,frame_spec[0]/2),(50,50), color=Color.GREEN, filled=True)
     img.addDrawingLayer(overlay)
     img.applyLayers()
 
 def draw_bar(img):
     overlay = DrawingLayer((img.width, img.height))
-    overlay.rectangle((0,230),(img.width,20), color=Color.RED, width=3)
+    overlay.rectangle((0,frame_floor),(img.width,bar_height), color=Color.RED, width=3)
     img.addDrawingLayer(overlay)
     img.applyLayers()
 
-
-
 cam = Camera()
 
-for i in range(80):
-	frame = cam.getImage().grayscale().binarize(45)
+for i in range(150):
+	frame = cam.getImage().scale(frame_spec[0], frame_spec[1]).grayscale().binarize(45)
 	gauge(frame)
 	draw_bar(frame)
 	draw_box(frame, get_avg_x(gauge(frame)))
